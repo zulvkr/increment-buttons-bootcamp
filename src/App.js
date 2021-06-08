8526dfbf343acdffa29e4edf6b9e584dc593db7b
zulvkr <ivan.zulf.ikar@gmail.com>
2021-06-08 22:20:20 +0700

    Add standard, finish app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Button from './components/Button'
 
-function App() {
+function App () {
+  const [count, setCount] = useState(0)
+
+  const incrementCount = increment => {
+    setCount(count + increment)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Button increment={1} onClickFunction={incrementCount} />
+      <Button increment={10} onClickFunction={incrementCount} />
+      <Button increment={100} onClickFunction={incrementCount} />
+      <Button increment={1000} onClickFunction={incrementCount} />
+      <span>{count}</span>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+const Button = ({ increment, onClickFunction }) => {
+  const handleClick = () => {
+    onClickFunction(increment)
+  }
+  return <button onClick={handleClick}>+{increment}</button>
+}
+
+export default Button
